fb3306b17c4b96977ae890798052528e1d01fa9f
Akai Shih <sakai@fortinet-us.com>
2026-02-19 09:23:19 +0800

    fix: Fix routes in adapter for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,20 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    adapter: cloudflare(),
+    adapter: cloudflare({
+        routes: {
+            extend: {
+                include: ['/*'],
+                exclude: [
+                    '/_astro/*',
+                    '/fonts/*',
+                    '/images/*',
+                    '/favicon.ico',
+                    '/sitemap.xml',
+                    '/robots.txt'
+                ]
+            }
+        }
+    }),
     integrations: [tailwind(), mdx()]
 });
